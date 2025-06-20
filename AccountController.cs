7467e69acd0df5df54216c287e267036f55af6b0
Nazarro <nazar.sopiha@gmail.com>
2025-06-19 23:36:04 -0400

    fix double response definition

--- AccountController.cs
+++ AccountController.cs
@@ -84,22 +84,4 @@ namespace EveAuthApi
             return Ok(users);
         }
     }
-
-    public class LoginResponse
-    {
-        public string AccessToken { get; set; } = "";
-        public string RefreshToken { get; set; } = "";
-        public int ExpiresIn { get; set; }
-        public string TokenType { get; set; } = "";
-        public DateTime IssuedAt { get; set; }
-        public UserInfo User { get; set; } = new();
-    }
-
-    public class UserInfo
-    {
-        public int Id { get; set; }
-        public string Email { get; set; } = "";
-        public bool IsSubscribed { get; set; }
-        public DateTime CreatedAt { get; set; }
-    }
 }
--- LoginResponse.cs
+++ LoginResponse.cs
@@ -2,17 +2,18 @@ namespace EveAuthApi
 {
     public class LoginResponse
     {
-        public string AccessToken { get; set; } = null!;
-        public string RefreshToken { get; set; } = null!;
+        public string AccessToken { get; set; } = "";
+        public string RefreshToken { get; set; } = "";
         public int ExpiresIn { get; set; }
-        public string TokenType { get; set; } = "Bearer";
-        public UserInfo User { get; set; } = null!;
+        public string TokenType { get; set; } = "";
+        public DateTime IssuedAt { get; set; }
+        public UserInfo User { get; set; } = new();
     }
 
     public class UserInfo
     {
         public int Id { get; set; }
-        public string Email { get; set; } = null!;
+        public string Email { get; set; } = "";
         public bool IsSubscribed { get; set; }
         public DateTime CreatedAt { get; set; }
     }
