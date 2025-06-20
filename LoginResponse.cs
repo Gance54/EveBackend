80cd6fa91eda62141dc701782ea12c89ecea7f94
Nazarro <nazar.sopiha@gmail.com>
2025-06-19 23:41:59 -0400

    fix loginResponce class by adding expiresIn

--- LoginResponse.cs
+++ LoginResponse.cs
@@ -4,7 +4,8 @@ namespace EveAuthApi
     {
         public string AccessToken { get; set; } = "";
         public string RefreshToken { get; set; } = "";
-        public int ExpiresIn { get; set; }
+        public int AccessTokenExpiresIn { get; set; } // 1 hour
+        public int RefreshTokenExpiresIn { get; set; } // 1 month
         public string TokenType { get; set; } = "";
         public DateTime IssuedAt { get; set; }
         public UserInfo User { get; set; } = new();
